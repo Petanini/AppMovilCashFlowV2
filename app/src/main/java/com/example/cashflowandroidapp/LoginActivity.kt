dd58b3b9baa02960e556770642c75460c72e997b
sebastian-huamani <huamanitassara@gmail.com>
2024-12-05 16:25:27 -0500

    eliminacion de Action Bar de las vistas

--- app/src/main/java/com/example/cashflowandroidapp/LoginActivity.kt
+++ app/src/main/java/com/example/cashflowandroidapp/LoginActivity.kt
@@ -33,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        supportActionBar?.hide()
         auth = Firebase.auth
 
 
